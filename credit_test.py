ebe2b5820e8d19e1f3c91a0e9790d65642b797d9
Chaewon <chaewon.lim2@gmail.com>
2022-10-07 23:47:49 -0400

    added some tests

--- credit_test.py
+++ credit_test.py
@@ -1,3 +1,4 @@
+from multiprocessing import set_start_method
 import credit
 import random
 
@@ -18,7 +19,7 @@ daysInMonth = {
 
 #Tester code to try to test the code.
 #Separate from the tester code found in credit.py
-#No tests created as of oct7.
+
 #idea: one section manually tests specific instances, and another section tries random valid entries.
 
 #return a real possible day given the month and year.
@@ -28,6 +29,21 @@ def randomDay(month, year):
         last_day_in_month = 29
     return random.randint(1,last_day_in_month)
 
+#return a real possible amount
+def randomAmount(upper_bound):
+    return round((random.random() * upper_bound), 2)
+
+# Simulate buying and paying back some random amount immediately n times in the same country on the same day.
+# Return true iff the amount owed does not change.
+def buyAndPay(day, month, country, n):
+    starting_cur = credit.amount_owed(day, month)
+    for i in range(n):
+        amount = randomAmount(credit.amount_owed()) 
+        credit.purchase(amount, day, month, country)
+        credit.pay_bill(amount, day, month, country)
+    if starting_cur == credit.amount_owed(day, month):
+        return True
+    return False
 
 
 if __name__ == '__main__':
